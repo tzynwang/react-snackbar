762174dda9a83378b7d8481f8baafb7b21d03905
Tzu Yin <tzyn.wang@gmail.com>
2022-09-04 21:04:38 +0800

    [feat] update alias

--- config/alias.js
+++ config/alias.js
@@ -2,4 +2,5 @@ const path = require('path');
 
 module.exports = {
   '@Components': path.resolve(__dirname, '..', './src/components'),
+  '@Models': path.resolve(__dirname, '..', './src/models'),
 };
