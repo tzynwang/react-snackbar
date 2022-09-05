1f35d2f6e10d545218c32170964ea756c83fdd97
Tzu Yin <tzyn.wang@gmail.com>
2022-09-05 08:47:16 +0800

    [feat] pause countdown when hover

--- src/components/Common/Snack/index.tsx
+++ src/components/Common/Snack/index.tsx
@@ -1,4 +1,4 @@
-import React, { memo, useEffect, useMemo, useState } from 'react';
+import React, { memo, useEffect } from 'react';
 import { css, keyframes } from '@emotion/css';
 import cn from 'classnames';
 import type { SnackProps } from './types';
@@ -23,89 +23,82 @@ function Snack(props: SnackProps): React.ReactElement {
     onClose,
     ...rest
   } = props;
+  const animationDuration = countDown / 1000;
   const classNameFromProps = rest.className;
   delete rest.className;
-  const animationDuration = countDown / 1000;
+
+  /* Functions */
+  const closeSnack = (): void => {
+    if (onClose) {
+      onClose();
+    }
+  };
 
   /* Hooks */
   useEffect(() => {
-    if (show && onClose) {
-      setTimeout(() => {
-        onClose();
-      }, countDown);
-    }
-  }, [show, countDown, onClose]);
-
-  /* Views */
-  const Snack = useMemo(
-    () =>
-      show ? (
-        <div
-          className={cn(
-            css({
-              minHeight: '60px',
-              minWidth: '240px',
-              display: 'inline-block',
-              position: 'fixed',
-              top: '24px',
-              right: '24px',
-              padding: '8px',
-              backgroundColor: '#fff',
-              '&:hover::before': {
-                animationPlayState: pauseOnHover ? 'paused' : 'running',
-              },
-              '&::before': {
-                content: '""',
-                width: '100%',
-                height: '2px',
-                position: 'absolute',
-                top: 0,
-                left: 0,
-                backgroundColor: '#4e342e',
-                transform: 'scaleX(0%)',
-                transformOrigin: 'top left',
-                animation: `${countDownAnimation} ${animationDuration}s ease`,
-              },
-            }),
-            classes.snack,
-            classNameFromProps
-          )}
-        >
-          {children}
-          <button
-            type="button"
-            onClick={onClose}
-            className={cn(
-              css({
-                width: '24px',
-                height: '24px',
-                position: 'absolute',
-                top: '8px',
-                right: '8px',
-                borderRadius: '24px',
-                border: 'none',
-                cursor: 'pointer',
-              }),
-              classes.closeButton
-            )}
-          >
-            X
-          </button>
-        </div>
-      ) : (
-        <React.Fragment />
-      ),
-    [
-      show,
-      animationDuration,
-      classes.snack,
-      classes.closeButton,
-      classNameFromProps,
-    ]
-  );
+    document.addEventListener('animationend', closeSnack);
+    return () => {
+      document.removeEventListener('animationend', closeSnack);
+    };
+  }, []);
 
   /* Main */
-  return Snack;
+  return show ? (
+    <div
+      className={cn(
+        css({
+          minHeight: '60px',
+          minWidth: '240px',
+          display: 'inline-block',
+          position: 'fixed',
+          top: '24px',
+          right: '24px',
+          padding: '8px',
+          backgroundColor: '#fff',
+          '&:hover::before': {
+            animationPlayState: pauseOnHover ? 'paused' : 'running',
+          },
+          '&::before': {
+            content: '""',
+            width: '100%',
+            height: '2px',
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            backgroundColor: '#4e342e',
+            transform: 'scaleX(0%)',
+            transformOrigin: 'top left',
+            animation: `${countDownAnimation} ${animationDuration}s ease`,
+          },
+        }),
+        classes.snack,
+        classNameFromProps
+      )}
+    >
+      {children}
+      <button
+        type="button"
+        onClick={onClose}
+        className={cn(
+          css({
+            width: '24px',
+            height: '24px',
+            position: 'absolute',
+            top: '8px',
+            right: '8px',
+            borderRadius: '24px',
+            border: 'none',
+            cursor: 'pointer',
+          }),
+          classes.closeButton
+        )}
+      >
+        X
+      </button>
+    </div>
+  ) : (
+    <React.Fragment />
+  );
 }
 
 export default memo(Snack);
